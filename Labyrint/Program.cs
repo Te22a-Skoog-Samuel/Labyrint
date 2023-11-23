0bb93e7662f559557080b0747ce200565c24b43c
Samuel Skoog <samule.skoog@elev.ga.ntig.se>
2023-11-23 11:01:35 +0100

    påbörja själva labyrinten

--- Labyrint/Program.cs
+++ Labyrint/Program.cs
@@ -1,16 +1,22 @@
-﻿Console.WriteLine("Du vaknar upp och inser att du MÅSTE!!! skita. ");
+﻿
+Console.WriteLine("Du vaknar upp och inser att du MÅSTE!!! skita. ");
 
 Console.WriteLine("Du öppnar sovrumsdörren och ser tre dörrar, hitta rätt väg till toan.");
 
 Console.WriteLine("Väljer du dör a, dörr b eller dörr c?");
 
-string dörr = "";
+string room = "Sovrum";
 
-if (dörr == "a")
-{
+while (room != "NetherRealm")
+if (room =="Sovrum");
+
+
+
+
+
 
 
     
-}
+
 
 Console.ReadLine();
