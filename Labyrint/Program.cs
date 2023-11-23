b5bce3bd77f5ca25a7de771583dc449721468afd
Samuel Skoog <samule.skoog@elev.ga.ntig.se>
2023-11-23 10:00:24 +0100

    introduktion

--- Labyrint/Program.cs
+++ Labyrint/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿Console.WriteLine("Du vaknar upp och inser att du MÅSTE!!! skita. ");
+
+Console.WriteLine("Du öppnar sovrumsdörren och ser tre dörrar, hitta rätt väg till toan.");
+
+Console.WriteLine("Väljer du dör a, dörr b eller dörr c?");
+
+string dörr = "";
+
+if (dörr == "a")
+{
+
+
+    
+}
+
+Console.ReadLine();
