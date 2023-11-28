7e71b26d4eedd74b75f888bd97d3724bee6d7cad
Samuel Skoog <samule.skoog@elev.ga.ntig.se>
2023-11-28 10:41:20 +0100

    skapat metod

--- Labyrint/Program.cs
+++ Labyrint/Program.cs
@@ -1,14 +1,30 @@
 ﻿
+static void nytt_rum()
+{
+Console.WriteLine("Vilken dörr väljer du?");
+}
+
 Console.WriteLine("Du vaknar upp och inser att du MÅSTE!!! skita. ");
-
 Console.WriteLine("Du öppnar sovrumsdörren och ser tre dörrar, hitta rätt väg till toan.");
-
 Console.WriteLine("Väljer du dör a, dörr b eller dörr c?");
 
 string room = "Sovrum";
 
 while (room != "NetherRealm")
-if (room =="Sovrum");
+{
+
+}
+
+if (room =="Sovrum")
+{
+    nytt_rum();
+}
+
+string dörr = Console.ReadLine();
+
+if (dörr == "a")
+room = "";
+
 
 
 
