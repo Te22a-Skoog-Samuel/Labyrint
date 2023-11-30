1b29a10430e204d841a6152d9770a0afc33d4d22
Samuel Skoog <samule.skoog@elev.ga.ntig.se>
2023-11-30 11:00:59 +0100

    Gjort en lista och ett till rum

--- Labyrint/Program.cs
+++ Labyrint/Program.cs
@@ -1,29 +1,52 @@
 ﻿
 static void nytt_rum()
 {
-Console.WriteLine("Vilken dörr väljer du?");
+Console.WriteLine("Vilken dörr väljer du? dörr a, dörr b eller dörr c");
 }
+
+List<string> visitedRooms = new();
 
 Console.WriteLine("Du vaknar upp och inser att du MÅSTE!!! skita. ");
 Console.WriteLine("Du öppnar sovrumsdörren och ser tre dörrar, hitta rätt väg till toan.");
-Console.WriteLine("Väljer du dör a, dörr b eller dörr c?");
 
 string room = "Sovrum";
 
 while (room != "NetherRealm")
 {
+   
+    if (room =="Sovrum")
+    {
 
+        if (visitedRooms.Contains("Sovrum"))
+        {
+            // Alla efterföljande
+            Console.WriteLine("Vad fan gör du här igen, behövde du inte skita eller?");
+            nytt_rum();
+        }
+        else 
+        {
+            // Första
+            visitedRooms.Add("Sovrum");
+
+            nytt_rum();
+        }
+    
+    }   
+
+    string dörr = Console.ReadLine();
+
+    if (dörr == "a")
+    {
+        room = "The Void";
+
+        Console.WriteLine("Det verkar som att du har brytit mot naturens lagar och warpat till ett ställe där tid och rum inte existerar.");
+        Console.WriteLine("Efter du har varit i detta tomrum i 370 år så hittar du tre teleportörer.");
+        Console.WriteLine("Väljer du teleportör a, b eller c.");
+    }
+    
 }
 
-if (room =="Sovrum")
-{
-    nytt_rum();
-}
 
-string dörr = Console.ReadLine();
-
-if (dörr == "a")
-room = "";
 
 
 
