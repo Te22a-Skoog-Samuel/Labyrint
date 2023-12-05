13143794cb377ff06bd188a9c4ce313cb2aad192
Samuel Skoog <samule.skoog@elev.ga.ntig.se>
2023-12-05 11:10:52 +0100

    Gjort klart ett till rum

--- Labyrint/Program.cs
+++ Labyrint/Program.cs
@@ -1,7 +1,7 @@
 ﻿
 static void nytt_rum()
 {
-Console.WriteLine("Vilken dörr väljer du? dörr a, dörr b eller dörr c");
+    Console.WriteLine("Vilken dörr väljer du? dörr a, dörr b eller dörr c?");
 }
 
 List<string> visitedRooms = new();
@@ -13,8 +13,8 @@ string room = "Sovrum";
 
 while (room != "NetherRealm")
 {
-   
-    if (room =="Sovrum")
+
+    if (room == "Sovrum")
     {
 
         if (visitedRooms.Contains("Sovrum"))
@@ -23,27 +23,40 @@ while (room != "NetherRealm")
             Console.WriteLine("Vad fan gör du här igen, behövde du inte skita eller?");
             nytt_rum();
         }
-        else 
+        else
         {
             // Första
             visitedRooms.Add("Sovrum");
 
             nytt_rum();
         }
+
+
+        string dörr = Console.ReadLine();
+
+        if (dörr == "a")
+        {
+            room = "The Void";
+
+            Console.WriteLine("Det verkar som att du har brytit mot naturens lagar och warpat till ett ställe där tid och rum inte existerar.");
+            Console.WriteLine("Efter du har varit i detta tomrum i 370 år så hittar du tre dörrar svävande.");
+            nytt_rum();
+
+        }
     
-    }   
+        else if (dörr == "b")
+        {
+            room = "Djungeln";
 
-    string dörr = Console.ReadLine();
+            Console.WriteLine("Nu verkar det som att du har hamnat i en djungel");
+            Console.WriteLine("Du går runt ett tag och från ingenstans så träffar du på Mowgli från djungelboken och han visar dig vägen till dörrarna");
+            nytt_rum();
 
-    if (dörr == "a")
-    {
-        room = "The Void";
-
-        Console.WriteLine("Det verkar som att du har brytit mot naturens lagar och warpat till ett ställe där tid och rum inte existerar.");
-        Console.WriteLine("Efter du har varit i detta tomrum i 370 år så hittar du tre teleportörer.");
-        Console.WriteLine("Väljer du teleportör a, b eller c.");
+        }
+    
+    
     }
-    
+
 }
 
 
@@ -55,7 +68,7 @@ while (room != "NetherRealm")
 
 
 
-    
+
 
 
 Console.ReadLine();
